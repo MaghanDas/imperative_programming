d7dade5e08835f1f646c21181ba9975e402bdf46
Maghan Das <dasmaghan786@gmail.com>
2024-10-11 13:08:35 +0000

    datatypes modified

---
+++ basics/variables.c
@@ -0,0 +1,17 @@
+
+#include<stdio.h>
+
+int main() {
+
+    char name[] = "Adam";
+    int age = 20;
+    char grade = 'A';
+    
+
+    printf( "Name : %s\n", name);
+    printf( "Age : %d\n", age);
+    printf( "Grade : %c\n", grade);
+
+    return 0;
+
+}
