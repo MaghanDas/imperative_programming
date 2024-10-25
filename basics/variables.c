fa2de90c791137ff1c4c9cc8dcd0b77e08d1d171
Maghan Das <dasmaghan786@gmail.com>
2024-10-25 16:57:46 +0000

    more on variables

--- basics/variables.c
+++ basics/variables.c
@@ -1,33 +1,52 @@
 
-#include<stdio.h>
+#include <stdio.h>
 
-int main() {
+int main()
+{
 
-    char name[] = "Adam";
-    int age = 20;
-    char grade = 'A';
-    
+    //  int age = 22;
+    //  int weight = 55;
+    // // by default it wil contain value 0;
+    // printf("%d", age );
+    //  printf("my age is %i ;", age + weight);
+    //  printf("my age is %i ;", ++age); // try this if age has no value assigned!
+    //  char name = 'm';
+    //  printf("my name is %c " , name);
 
-    printf( "Name : %s\n", name);
-    printf( "Age : %d\n", age);
-    printf( "Grade : %c\n", grade);
+    // *************************************
+    //  working with datatypes...
+    // int contain 2 or 4 bytes, charcater with one byte and float 4.
+    // format specifiers are %i,%d for int , %f for floats , %lf for doubles. %c for characters.
 
+    /// character datatype ..
+    // char grade = 'A';
+    // char remarks = 65;
+    // char mytext[] = "hello"; // it will print whole but specifier should be %s.
+    // char text = 'hello';     // it will only print the last letter of the word and shows warninig as well.
+    // printf("My grade in maths is %c", grade);
+    // printf("\nMy grade in maths is %c", remarks);
+    // printf("%c", text);     // 0
+    // printf("\n%s", mytext); // hello
 
-// working with input:
+    // working with numbers...
+    //     float num = 3.2222;
+    //    printf("%.2f",num);  // .2 mean only 2 digits after decimal..
 
-//   int input;
-//   scanf( "%i", &input);
-//   printf( "you entered : %i", input);
+    // char grade = 'A';
+    // int num = 23;
+    // float num2 = 2.33;
+    // double digit = 32.33;
 
-  char inputChar;
-  scanf( "%c", &inputChar);
-  printf( "you entered : %c ", inputChar); // char
-  printf( "you entered : %i ", inputChar); // ascii value
+    // using %lu when we want the result of something,,
+    // printf("size of char %lu\n", sizeof(grade));   // 1
+    // printf("size of int  %lu\n", sizeof(num));     // 4
+    // printf("size of float %lu\n", sizeof(num2));   // 4
+    // printf("size of double %lu\n", sizeof(digit)); // 4
+
+    // int inputNumber;
+    // scanf("%i", &inputNumber);
+    // printf("adress: %p inputNumber: %i\n", &inputNumber, inputNumber);
+
 
     return 0;
-
-
-
-
-
 }
